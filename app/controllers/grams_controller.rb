957e4c0e6010622863d7286f482ebb9e593cfb4a
cw2981 <cw2981>
2019-08-12 20:28:50 +0000

    lesson 12 complete, both tests returning 302 unknown reason. moving to next lesson.

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,13 +1,19 @@
 class GramsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :show]
   
   def new
     @gram = Gram.new
   end
 
   def index
-
   end
+  
+  def show
+    @gram = Gram.find_by_id(params[:id])
+    if @gram.blank?
+    render plain: 'Not Found :(', status: :not_found
+  end
+end
 
   def create
      @gram = current_user.grams.create(gram_params)
